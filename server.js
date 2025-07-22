5155726e5c5ac7b23018506a3bc6b6fb7d3013da
britto <matheus@stssistemas.com.br>
2025-07-21 22:54:27 -0300

    ghj

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 80;
 
 // Middleware para JSON
 app.use(express.json({ limit: '50mb' }));
